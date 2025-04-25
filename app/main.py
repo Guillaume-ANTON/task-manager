02b2e384086e9242c18c84f01fc99fe7efe11fd6
Guillaume Anton <guillaumeanton@MacBook-Pro-de-Guillaume.local>
2025-04-25 10:20:12 +0200

    feat(core): setup FastAPI entrypoint and database configuration

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from app.routes import users
+from app.database import Base, engine
+
+app - FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(users.router)
